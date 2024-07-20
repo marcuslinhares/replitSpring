8888f6063d7e11ca4453eece4577c9b706a285e2
marcuslinhares <marcusviniciuslinhares543@gmail.com>
2024-07-20 10:39:51 -0300

    first commit

---
+++ src/main/java/dev/spring/replit/spring_replit/SpringReplitApplication.java
@@ -0,0 +1,27 @@
+package dev.spring.replit.spring_replit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SpringReplitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReplitApplication.class, args);
+	}
+
+	@RestController
+	@RequestMapping(path = "/api")
+	public class SomaController{
+
+		@GetMapping
+		public ResponseEntity<String> echo(){
+			return ResponseEntity.ok().body("api no ar");
+		}
+	}
+
+}
---
+++ src/test/java/dev/spring/replit/spring_replit/SpringReplitApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.spring.replit.spring_replit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReplitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
